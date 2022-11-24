9dd5bc24cd8fd92ee4cbe5e85c63477da818b764
pawel.trzcinski <pawel.trzcinski@onet.eu>
2022-11-24 17:01:41 +0100

    Next iteration suggestions

--- src/ClassNamer/Program.cs
+++ src/ClassNamer/Program.cs
@@ -3,6 +3,13 @@ using log4net;
 
 namespace ClassNamer
 {
+	// TODO: 
+	// It would be good to have some optional pre-conditions (e.g. a desired purpose of the class like "factory" or "service" or a minimum number of words that we want to use)
+	// Language selector (e.g. when using Java names are going to be 10x longer)
+	// Input field with a "required" keyword that we want to include in the generator
+	// Choosing your desired seniority level (e.g. on intern level you would get mixed PascalCase, snake_case etc.)
+	// Obscenity level? If we want some additional swear words
+	
     /// <summary>
     /// Main program class.
     /// </summary>
